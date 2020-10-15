7b68aa1afb982ca7d0d24ad8ead48f365416c706
0kira <okira@hotmail.be>
2020-10-15 18:19:40 +0200

    GRAVITY (fck it)

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,94 +1,61 @@
+'use strict';
 import gsap from 'gsap';
 import { _getProperty } from 'gsap/gsap-core';
 let space_bar = document.querySelector('.space-bar');
 let fusee = document.querySelector('.fusee-js');
-
-
 let i = 0;
 
-
-
-// Faire descendre la fusée si on reste pas appuyé
 function fuseeAnimation() {
-    // Etape 1
-    if(i < 25) {
+    // Etape 1 : sortie de l'atmosphere
+    if(i < 20) {
         let posY = -20;
         gsap.to(".fusee-js", {
             y:"+="+posY,
             duration: 1,
-            rotate: "8deg"
+            rotate: "8deg",
         });
-        setTimeout(() => {
-    /*         gsap.to(".fusee-js", {
-                y:"-="+accumulate,
-                duration: 1,
-                rotate: "8deg"
-            }); */
-
-            var transformValues = window.getComputedStyle(fusee).getPropertyValue("transform").match(/(-?[0-9\.]+)/g);
-            let distance = transformValues[5];
-            console.log(distance);
-                
-
-        }, 1000);
-
-
-        // mouvement perpetuel
     }
-    else if(i < 35) {
+    // Etape 2 : dans l'espace
+    else if(i < 30) {
         let posY = -1000;
         gsap.to(".fusee-js", {
             y:"+="+posY,
             duration: 1,
         });
-        console.log(i);
         // Change stage objectif
-
     }
-    else if(i < 35) {
-        console.log('bloqué');
-        // Mission complete
-
-    }
-
-/*             // Etape 2
-    gsap.to(".fusee-js", {
-        y:"-="+posY,
-        duration: 1,
-        rotate:"-85deg",
-    });
-
-
- */
     i++;
-
 }
-
-function SpaceBarAnimation() {
-}
-
-
-// Detect space bar 
 
 
 window.onkeypress = (e) => {
     if(e.keyCode == 32) {
         fuseeAnimation()
-        SpaceBarAnimation()
         gsap.to(".space-bar", 0.3, {
             opacity:1
         });
-        SpaceBarAnimation()
     } 
 }
 
-
 window.onkeyup = (e) => {
-    gsap.to(".space-bar", 0.3, {
-        opacity:0.5
-    });
+    if(e.keyCode == 32) {
+        gsap.to(".space-bar", 0.3, {
+            opacity:0.5
+        });
+    }
+    if(i < 20) {
+        setTimeout(() => {
+            var transformValues = window.getComputedStyle(fusee).getPropertyValue("transform").match(/(-?[0-9\.]+)/g);
+            let distance = transformValues[5];
+            console.log(distance);
+            gsap.to('.fusee-js', 2, {
+                y:0
+            });
+            i = 0;
+        }, 500);
+    }
 }
-    
+
 
 
 

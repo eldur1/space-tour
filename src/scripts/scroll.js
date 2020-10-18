6e5344492847b0cee83bd2adf810e1097425739d
Tom_Delfosse <Tom-Delfosse@users.noreply.github.com>
2020-10-18 14:40:08 +0200

    changement modèle fusée + smooth scroll

--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -1,5 +1,6 @@
+"use strict";
 const btnScroll = document.querySelector(".btn-scroll");
 const ScrollTo = document.querySelector(".section--what");
 btnScroll.addEventListener("click", (e) => {
-    zenScroll.to(ScrollTo)
+    zenscroll.to(ScrollTo, 800);
 })

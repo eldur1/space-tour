a96388e9d755c5550ae4b3a495b75e906e1a2892
Tom_Delfosse <Tom-Delfosse@users.noreply.github.com>
2020-11-09 14:36:18 +0100

    hotfix

--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -1,5 +1,5 @@
 "use strict";
-const btnScroll = document.querySelector(".btn-scroll");
+const btnScroll = document.querySelector(".btn--scroll");
 const ScrollTo = document.querySelector(".section--what");
 btnScroll.addEventListener("click", (e) => {
     zenscroll.to(ScrollTo, 800);
